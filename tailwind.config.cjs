4859e39f0bf3984bdabbb0f33ad739015c3311fb
laerke <laer192c@stud.kea.dk>
2022-08-28 17:12:40 +0200

    nye ændringer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  darkMode: 'class',
 
   theme: {
     colors: {
-      primary: "gray-300",
-      accent: "",
+      'blue': "#D0D8D9",
+      'dark-blue': "#939899",
+      'dark': "#e5e7eb",
+      'black': "#000000",
+      'white': "#FCFCFC",
+      'broken-white': "#f8fafc",
+      'orange': "#F2E5D5",
     },
-  
+
     extend: {
+      colors: {
+       
+
+      },
     fontFamily: {
-    h1: "poppins,system-ui, sans-serif",
+    h1: "",
   },
 },
 },
 
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
+
